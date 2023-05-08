cca64b2ed8f09b1f482014e5e13a947c8239a757
diegorca23 <diego.rca.23@gmail.com>
2023-05-08 19:47:30 -0300

    Test de ejemplo

--- coder/tests.py
+++ coder/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+import random
+import string
 
-# Create your tests here.
+from django.test import TestCase
+from coder.models import Curso
+
+class CursoTestCase(TestCase):
+
+    def test_creacion_cursos(self):
+        # Test 1: Comprobar puedo crear un curso con un nombre con letras random
+        lista_letras_comision = [random.choice(string.ascii_letters + string.digits) for _ in range(20)]
+        lista_letras_lenguaje = [random.choice(string.ascii_letters + string.digits) for _ in range(20)]
+        comision_prueba = "".join(lista_letras_comision)
+        lenguaje_prueba = "".join(lista_letras_lenguaje)
+        
+        
+        curso_1 = Curso.objects.create(comision=comision_prueba, lenguaje=lenguaje_prueba)
+
+        self.assertIsNotNone(curso_1.id)
+        self.assertEqual(curso_1.comision, comision_prueba)
+        self.assertEqual(curso_1.lenguaje, "Diego")
