946f0510a8393e1d38e54a6192c3b7b1c5218b2f
diegorca23 <diego.rca.23@gmail.com>
2023-05-08 19:23:30 -0300

    Edición de Usuario y Avatar

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('registro/', views.registro, name='registro'),
     path('logout/', LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
+    path('editar-perfil/', views.editar_perfil, name='editar_perfil'),
+    path('cambio-contrasenia/', views.CambioContrasenia.as_view(), name='cambio_contrasenia')
 ]
